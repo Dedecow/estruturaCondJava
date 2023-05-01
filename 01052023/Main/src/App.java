02f38a5664b5ac27e233a7c0c2414ad2281e0ba5
André Silveira <114875096+Dedecow@users.noreply.github.com>
2023-05-01 12:22:28 -0300

    Add files via upload

---
+++ 01052023/Main/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int idade = 20; 
+        if (idade > 18){
+             System.out.println("Você é maior de idade. ");
+        }else{
+            System.out.println("Você é menor de idade. ");
+        }
+       
+    }
+}
---
+++ 01052023/Main/src/app2.java
@@ -0,0 +1,26 @@
+public class app2 {
+    public static void main(String[] args) throws Exception {
+        int diaDaSemana = 7; 
+        switch (diaDaSemana){
+            case 1:
+            System.out.println("Hoje é Segunda-Feira. ");
+            break;
+            case 2:
+            System.out.println("Hoje é Terça-Feira. ");
+            break;
+            case 3:
+            System.out.println("Hoje é Quarta-Feira. ");
+            break;
+            case 4:
+            System.out.println("Hoje é Quinta-Feira. ");
+            break;
+            case 5:
+            System.out.println("Hoje é Sexta-Feira. ");
+            break;
+            default: 
+            System.out.println("Não é dia útil");
+        }
+       
+    }
+}  
+
